5ea9de34df030eedb3c2c946d70488937e4e6cca
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-10-03 16:29:52 +0530

    Adding 'bcryptJs' for encrypting the password & hashing pw while user registers.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,7 @@
 import express from 'express';
-import { User } from '../models/userModel.js';
-
 const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Users screen');
-});
+import { User } from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
 
 router.post('/register', (req, res) => {
   const { name, username, email, password } = req.body;
@@ -14,29 +10,32 @@ router.post('/register', (req, res) => {
       .status(422)
       .json({ error: 'Please add all the required fields.' });
   }
-  User.findOne({ email })
-    .then((savedUser) => {
-      if (savedUser) {
-        return res.status(422).json({ error: 'User already exists.' });
-      }
-      const user = new User({
-        name,
-        username,
-        email,
-        password,
-      });
-      user
-        .save()
-        .then((user) => {
-          res.json({ message: 'Saved successfully.' });
-        })
-        .catch((err) => {
-          console.log(err);
+  User.findOne({ email }).then((savedUser) => {
+    if (savedUser) {
+      return res.status(422).json({ error: 'User already exists.' });
+    }
+    bcrypt
+      .hash(password, 5)
+      .then((hashedPassword) => {
+        const user = new User({
+          name,
+          username,
+          email,
+          password: hashedPassword,
         });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+        user
+          .save()
+          .then((user) => {
+            res.json({ message: 'Saved successfully.' });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
 
 export default router;
