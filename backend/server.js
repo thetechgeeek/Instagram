6b35a80b7b03e925fc72316a10913ed5ebc643d2
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-10-03 15:36:12 +0530

    Creating 'db.js' for connecting to mongoDB & adding it to 'server.js'

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB();
 
 const app = express();
 app.get('/', (req, res) => {
