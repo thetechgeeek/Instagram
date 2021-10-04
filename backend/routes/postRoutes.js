dfea2ccdcf19b36c02f02d09d9eebace1b9947b2
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-10-04 16:24:22 +0530

    Added '/allposts' & '/myposts' routes

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -3,6 +3,27 @@ const router = express.Router();
 import Post from '../models/postModel.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 
+router.get('/allposts', (req, res) => {
+  Post.find({})
+    .populate('postedBy', '_id name username')
+    .then((posts) => {
+      res.json({ posts });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get('/myposts', authMiddleware, (req, res) => {
+  Post.find({ postedBy: req.user._id })
+    .populate('postedBy', '_id name username')
+    .then((myPost) => {
+      res.json({ myPost });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 router.post('/createpost', authMiddleware, (req, res) => {
   const { title, body } = req.body;
   if (!title || !body) {
