4b97ae9753290dc0ac77b04e6f85ae14bbc5f483
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-10-04 23:04:51 +0530

    Adding 'react-router-dom' and attaching components/screens to appJs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Route path='/' component={HomeScreen} exact />
+      <Route path='/register' component={RegisterScreen} exact />
+      <Route path='/login' component={LoginScreen} exact />
+      <Route path='/profile' component={ProfileScreen} exact />
+    </Router>
+  );
 }
 
 export default App;
