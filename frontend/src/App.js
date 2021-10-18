5e1171c6598e621ba7d9cbb1752230db7e5d50a6
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-10-18 19:02:26 +0530

    Creating the User Context & it's state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,44 @@
+import React, { useEffect, createContext, useReducer, useContext } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, useHistory } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import LoginScreen from './screens/LoginScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import CreatePostScreen from './screens/CreatePostScreen';
-function App() {
+import { reducer, initialState } from './reducers/userReducer';
+export const UserContext = createContext();
+
+const Routing = () => {
+  const history = useHistory();
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      dispatch({ type: 'USER', payload: user });
+      history.push('/');
+    } else {
+      history.push('/login');
+    }
+  });
   return (
-    <Router>
+    <>
       <Route path='/' component={HomeScreen} exact />
       <Route path='/register' component={RegisterScreen} />
       <Route path='/login' component={LoginScreen} />
       <Route path='/profile' component={ProfileScreen} exact />
       <Route path='/post/create' component={CreatePostScreen} exact />
-    </Router>
+    </>
+  );
+};
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      <Router>
+        <Routing />
+      </Router>
+    </UserContext.Provider>
   );
 }
 
