1201b125d5ef805d6d26cd55e09b3552db7325c0
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-10-23 16:05:05 +0530

    Created the Follow/Unfollow backend pipeline

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 });
 
 const User = mongoose.model('User', userSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -92,4 +92,65 @@ router.get('/user/:id', authMiddleware, (req, res) => {
       return res.status(404).json({ error: 'User not found.' });
     });
 });
+
+router.put('/follow', authMiddleware, (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.followId,
+    {
+      $push: { followers: req.user._id },
+    },
+    {
+      new: true,
+    },
+    (err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      }
+      User.findByIdAndUpdate(
+        req.user._id,
+        {
+          $push: { following: req.body.followId },
+        },
+        { new: true }
+      )
+        .select('-password')
+        .then((result) => {
+          res.json(result);
+        })
+        .catch((err) => {
+          return res.status(422).json({ error: err });
+        });
+    }
+  );
+});
+router.put('/unfollow', authMiddleware, (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.unfollowId,
+    {
+      $pull: { followers: req.user._id },
+    },
+    {
+      new: true,
+    },
+    (err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      }
+      User.findByIdAndUpdate(
+        req.user._id,
+        {
+          $pull: { following: req.body.unfollowId },
+        },
+        { new: true }
+      )
+        .select('-password')
+        .then((result) => {
+          res.json(result);
+        })
+        .catch((err) => {
+          return res.status(422).json({ error: err });
+        });
+    }
+  );
+});
 export default router;
